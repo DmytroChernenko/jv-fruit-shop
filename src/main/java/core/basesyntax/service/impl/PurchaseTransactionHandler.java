01d16779c2514969303c326b55c66e7a2be5c841
mitresko <mitresko12@gmail.com>
2026-05-05 10:55:53 +0300

    clean code

--- src/main/java/core/basesyntax/service/impl/PurchaseTransactionHandler.java
+++ src/main/java/core/basesyntax/service/impl/PurchaseTransactionHandler.java
@@ -1,10 +1,11 @@
-package core.basesyntax.service;
+package core.basesyntax.service.impl;
 
 import core.basesyntax.model.Fruit;
 import core.basesyntax.model.FruitTransaction;
+import core.basesyntax.service.TransactionHandler;
 import core.basesyntax.storage.FruitStorage;
 
-public class PurchaseTransactionHandler implements TransactionHandler{
+public class PurchaseTransactionHandler implements TransactionHandler {
 
     private FruitStorage storage;
 
