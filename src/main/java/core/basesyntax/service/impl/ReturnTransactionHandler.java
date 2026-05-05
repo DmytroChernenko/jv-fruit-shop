db5f2620bd4758e40b66e71e18a8c1d89474c0ea
mitresko <mitresko12@gmail.com>
2026-05-05 12:25:29 +0300

    implement transactions

--- src/main/java/core/basesyntax/service/impl/ReturnTransactionHandler.java
+++ src/main/java/core/basesyntax/service/impl/ReturnTransactionHandler.java
@@ -1,11 +1,19 @@
 package core.basesyntax.service.impl;
 
+import core.basesyntax.model.Fruit;
 import core.basesyntax.model.FruitTransaction;
 import core.basesyntax.service.TransactionHandler;
+import core.basesyntax.storage.FruitStorage;
 
 public class ReturnTransactionHandler implements TransactionHandler {
+    private FruitStorage storage;
+
+    public ReturnTransactionHandler(FruitStorage storage) {
+        this.storage = storage;
+    }
+
     @Override
     public void handleTransaction(FruitTransaction transaction) {
-
+        storage.returnFruit(new Fruit(transaction.getFruit()), transaction.getQuantity());
     }
 }
--- src/main/java/core/basesyntax/service/impl/SupplyTransactionHandler.java
+++ src/main/java/core/basesyntax/service/impl/SupplyTransactionHandler.java
@@ -1,11 +1,19 @@
 package core.basesyntax.service.impl;
 
+import core.basesyntax.model.Fruit;
 import core.basesyntax.model.FruitTransaction;
 import core.basesyntax.service.TransactionHandler;
+import core.basesyntax.storage.FruitStorage;
 
 public class SupplyTransactionHandler implements TransactionHandler {
+    private FruitStorage storage;
+
+    public SupplyTransactionHandler(FruitStorage storage) {
+        this.storage = storage;
+    }
+
     @Override
     public void handleTransaction(FruitTransaction transaction) {
-
+        storage.supply(new Fruit(transaction.getFruit()), transaction.getQuantity());
     }
 }
