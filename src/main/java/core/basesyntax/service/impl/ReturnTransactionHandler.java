dfc92d194844ddd60db1370c8247fee924f9f4f2
mitresko <mitresko12@gmail.com>
2026-05-05 10:53:41 +0300

    add transaction handlers without impl

---
+++ src/main/java/core/basesyntax/service/impl/ReturnTransactionHandler.java
@@ -0,0 +1,11 @@
+package core.basesyntax.service.impl;
+
+import core.basesyntax.model.FruitTransaction;
+import core.basesyntax.service.TransactionHandler;
+
+public class ReturnTransactionHandler implements TransactionHandler {
+    @Override
+    public void handleTransaction(FruitTransaction transaction) {
+
+    }
+}
---
+++ src/main/java/core/basesyntax/service/impl/SupplyTransactionHandler.java
@@ -0,0 +1,11 @@
+package core.basesyntax.service.impl;
+
+import core.basesyntax.model.FruitTransaction;
+import core.basesyntax.service.TransactionHandler;
+
+public class SupplyTransactionHandler implements TransactionHandler {
+    @Override
+    public void handleTransaction(FruitTransaction transaction) {
+
+    }
+}
