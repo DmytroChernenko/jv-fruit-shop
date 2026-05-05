7fb1e4b18c8a0ae6454c4b04ddfd6e68966f98cc
mitresko <mitresko12@gmail.com>
2026-05-05 16:55:25 +0300

    operationhandler map removed from handler

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -4,19 +4,34 @@ import core.basesyntax.db.FruitStorage;
 import core.basesyntax.db.FruitStorageImpl;
 import core.basesyntax.db.FruitTransactionDao;
 import core.basesyntax.db.FruitTransactionDaoCsvImpl;
+import core.basesyntax.model.FruitTransaction;
 import core.basesyntax.service.BalanceReporter;
 import core.basesyntax.service.OperationProcessor;
 import core.basesyntax.service.impl.CsvBalanceReporterImpl;
 import core.basesyntax.service.impl.OperationProcessorImpl;
+import core.basesyntax.strategy.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
         FruitTransactionDao dao = new FruitTransactionDaoCsvImpl();
-        dao.getAll().forEach(System.out::println);
 
         FruitStorage fruitStorage = new FruitStorageImpl();
 
-        OperationProcessor processor = new OperationProcessorImpl(fruitStorage);
+        Map<FruitTransaction.Operation, OperationHandler> operationOperationHandlerMap = new HashMap<>();
+
+        operationOperationHandlerMap.put(FruitTransaction.Operation.BALANCE,
+                new BalanceOperationHandler(fruitStorage));
+        operationOperationHandlerMap.put(FruitTransaction.Operation.SUPPLY,
+                new SupplyOperationHandler(fruitStorage));
+        operationOperationHandlerMap.put(FruitTransaction.Operation.RETURN,
+                new ReturnOperationHandler(fruitStorage));
+        operationOperationHandlerMap.put(FruitTransaction.Operation.PURCHASE,
+                new PurchaseOperationHandler(fruitStorage));
+
+        OperationProcessor processor = new OperationProcessorImpl(operationOperationHandlerMap);
         processor.processAll(dao.getAll());
 
         BalanceReporter reporter = new CsvBalanceReporterImpl(fruitStorage);
--- src/main/java/core/basesyntax/service/impl/OperationProcessorImpl.java
+++ src/main/java/core/basesyntax/service/impl/OperationProcessorImpl.java
@@ -1,11 +1,8 @@
 package core.basesyntax.service.impl;
 
-import core.basesyntax.db.FruitStorage;
 import core.basesyntax.model.FruitTransaction;
 import core.basesyntax.strategy.*;
 import core.basesyntax.service.OperationProcessor;
-
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -13,19 +10,9 @@ public class OperationProcessorImpl implements OperationProcessor {
 
     private Map<FruitTransaction.Operation, OperationHandler> transactionHandlerMap;
 
-    private FruitStorage fruitStorage;
 
-    public OperationProcessorImpl(FruitStorage fruitStorage) {
-        this.transactionHandlerMap = new HashMap<>();
-        this.fruitStorage = fruitStorage;
-        transactionHandlerMap.put(FruitTransaction.Operation.BALANCE,
-                new BalanceOperationHandler(fruitStorage));
-        transactionHandlerMap.put(FruitTransaction.Operation.SUPPLY,
-                new SupplyOperationHandler(fruitStorage));
-        transactionHandlerMap.put(FruitTransaction.Operation.RETURN,
-                new ReturnOperationHandler(fruitStorage));
-        transactionHandlerMap.put(FruitTransaction.Operation.PURCHASE,
-                new PurchaseOperationHandler(fruitStorage));
+    public OperationProcessorImpl(Map<FruitTransaction.Operation, OperationHandler> transactionHandlerMap) {
+        this.transactionHandlerMap = transactionHandlerMap;
     }
 
     @Override
